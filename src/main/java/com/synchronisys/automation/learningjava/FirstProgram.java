51cd6e04bfb37b6afbd8fd49347ed946d18860f4
Ashish <ashishkc2@gmail.com>
2015-08-12 18:50:31 -0400

    My first commit to learn java

---
+++ src/main/java/com/synchronisys/automation/learningjava/FirstProgram.java
@@ -0,0 +1,12 @@
+package com.synchronisys.automation.learningjava;
+
+/**
+ * Created by Ashish on 8/12/2015.
+ */
+public class FirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+
+    }
+}
